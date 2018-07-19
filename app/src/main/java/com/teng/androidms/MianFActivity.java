660ecb99d7c1756b50892477d8c2bd6ba019d8cf
王腾 <wangteng@jiedaibao.com>
2018-07-19 10:34:35 +0800

    okHttp 总结1

--- app/src/main/java/com/teng/androidms/MianFActivity.java
+++
@@ -1,4 +0,0 @@
-package com.teng.androidms;
-
-public class MianFActivity {
-}
--- app/src/main/java/com/teng/androidms/java/SingleInstance.java
+++ app/src/main/java/com/teng/androidms/java/SingleInstance.java
@@ -12,17 +12,18 @@ public class SingleInstance {
 
     ////////////////////////////////////////////////////////////////
     // 单例
-    // TODO: 17/7/29  懒汉式：只有调用getInstance()才会创建单例
+    /** TODO: 17/7/29  懒汉式：只有调用getInstance()才会创建单例
     private static SingleInstance singleInstance = null;
     public static SingleInstance getInstance(){
         if (singleInstance == null){
             singleInstance = new SingleInstance();
         }
         return singleInstance;
-    }
+    }**/
 
     //懒汉式加锁
-    // TODO 做了两次null检查 DCL(double check lock) ，确保了只有第一次调用单例的时候才会做同步，这样也是线程安全的，同时避免了每次都同步的性能损耗
+    // TODO 做了两次null检查 DCL(double check lock) ，确保了只有第一次调用单例的时候才会做同步，
+    // TODO 这样也是线程安全的，同时避免了每次都同步的性能损耗
     private static volatile SingleInstance singleInstance = null;
     // 如果不加 volatile 这种方式在可能造成失效
     // new SingleInstance() 会做三个操作，
@@ -36,6 +37,7 @@ public class SingleInstance {
     // A: 在java 1.5 的时候加了volatile，这个变量相当于加了synchronized ，
     // 这个变量保证了变量是从主存中读写的，而不是工作内存，而且读必须在写发生之后，
     // 所以对象的状态是所有对象都可见的
+/**
     public static SingleInstance getInstance(){
         if (singleInstance == null){
             synchronized (SingleInstance.class){
@@ -47,26 +49,27 @@ public class SingleInstance {
 
         return singleInstance;
     }
-
+**/
     // 懒汉式改进版
     // TODO 利用了classloader的机制来保证初始化instance时只有一个线程，所以也是线程安全的，同时没有性能损耗
+    /**
     private static class LazyLoader{
         private static final SingleInstance Instance = new SingleInstance();
     }
     public static final SingleInstance getInstance(){
         return LazyLoader.Instance;
     }
-
+    **/
 
     ////////////////////////////////////////////////////////////////
     // TODO: 17/7/29  饿汉式 线程安全的
+    /**
     private static final SingleInstance SINGLE_INSTANCE = new SingleInstance();
 
     public static SingleInstance getInstance(){
         return singleInstance;
     }
-
-
+    **/
 
 
 
