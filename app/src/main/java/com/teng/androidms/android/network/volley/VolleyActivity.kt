0db09f0340de7f948ff747084900c76643ce9232
王腾 <wangteng@jiedaibao.com>
2018-08-22 11:00:39 +0800

    Add Volley 模块

---
+++ app/src/main/java/com/teng/androidms/android/network/volley/VolleyActivity.kt
@@ -0,0 +1,13 @@
+package com.teng.androidms.android.network.volley
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.teng.androidms.R
+
+class VolleyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volley)
+    }
+}
